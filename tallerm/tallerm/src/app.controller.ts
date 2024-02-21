b472dddd6d6bd340db0c07f0bbe7e3f4e7be611b
yamilet-vas <yamilet.vazquez69@unach.mx>
2024-02-21 16:44:43 -0600

    Examen

---
+++ tallerm/tallerm/src/app.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get, Render } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  getHello(): any {
+    throw new Error('Method not implemented.');
+  }
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  @Render('index')
+  root() {
+    return { message: 'Bienvenidos al curso m'};
+  } 
+}
